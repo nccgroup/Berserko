eef0699e4762419634bfaa6567b0472e1c0693c3
Richard.Turnbull <richard.turnbull@nccgroup.com>
2017-01-26 16:57:59 +0000

    Bumped to version 0.9

--- berserko/src/burp/BurpExtender.java
+++ berserko/src/burp/BurpExtender.java
@@ -92,7 +92,7 @@ public class BurpExtender implements IBurpExtender, IHttpListener, ITab,
 	private boolean gotTGT = false;
 
 	private final String extensionName = "Berserko";
-	private final String versionString = "0.81 (alpha)";
+	private final String versionString = "0.9 (beta)";
 	private final String tabName = "Berserko";
 
 	private List<String> workingSet = null;
